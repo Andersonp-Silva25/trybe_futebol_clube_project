c639b58778739a8e0619de4cb2c65ba7268b6129
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-01-23 20:33:16 -0300

    :sparkles: Criando middleware para verificar se o email foi passado para o endpoint /login

--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import validateLogin from '../middleware/validateLogin';
 import LoginController from '../controllers/login.controller';
 
 const router = Router();
 const login = new LoginController();
 
-router.post('/', login.LoginUser);
+router.post('/', validateLogin, login.LoginUser);
 
 export default router;
