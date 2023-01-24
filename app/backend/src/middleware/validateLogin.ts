38b88ffb948a24dc97db0f2aefea75a4032f148d
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-01-23 21:43:41 -0300

    :hammer: Refactor: Adicionando verificação se existe senha no endpoint /login

--- app/backend/src/middleware/validateLogin.ts
+++ app/backend/src/middleware/validateLogin.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 
 const validateLogin = (req: Request, res: Response, next: NextFunction) => {
-  const { email } = req.body;
-  if (!email) {
+  const { email, password } = req.body;
+  if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
   return next();
